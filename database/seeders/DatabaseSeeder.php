3f490673d94c0c8a622336cc2df8aea01a36d8ee
Ayann12 <tantowi818@gmail.com>
2026-09-09 13:42:09 +0700

    Create database && akun admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Hashing\BcryptHasher;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +18,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@bc.com',
+            'password' => bcrypt('password'),
+            'role' => 'admin',
         ]);
     }
 }

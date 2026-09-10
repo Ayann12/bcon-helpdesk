474d9f235f927689d1c96309343d03e7909a7342
Ayann12 <tantowi818@gmail.com>
2026-09-10 15:27:40 +0700

    add ticket register

---
+++ app/Http/Controllers/TicketController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TicketStoreRequest;
+use App\Http\Resources\TicketResource;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Ticket;
+
+class TicketController extends Controller
+{
+    public function index(Request $request)
+    {
+        try {
+            $query = Ticket::query();
+
+            $query->orderBy('created_at', 'desc');
+
+            if ($request->search) {
+                $query->where('code', 'like', '%' . $request->search . '%')
+                    ->orWhere('title' . 'like', '%' . $request->search . '%');
+            }
+
+            if ($request->status) {
+                $query->where('status', $request->status);
+            }
+
+            if ($request->priority) {
+                $query->where('priority', $request->priority);
+            }
+
+            if (auth()->user()->role == 'user') {
+                $query->where('user_id', auth()->user()->id);
+            }
+
+            $tickets = $query->get();
+
+            return response()->json([
+                'message' => 'Data Tiket Berhasil Ditampilkan',
+                'data' => TicketResource::collection($tickets)
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Terjadi Kesalahan',
+                'data' => null
+            ], 500);
+        }
+    }
+
+    public function store(TicketStoreRequest $request)
+    {
+        $data = $request->validated();
+
+        DB::beginTransaction();
+
+        try {
+            $ticket = new Ticket;
+            $ticket->user_id = auth()->user()->id;
+            $ticket->code = 'TIC-' . rand(10000, 99999);
+            $ticket->title = $data['title'];
+            $ticket->description = $data['description'];
+            $ticket->priority = $data['priority'];
+            $ticket->save();
+
+            DB::commit();
+
+            return response()->json([
+                'message' => 'Ticket berhasil ditambahkan',
+                'data' => new TicketResource($ticket)
+            ], 201);
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+
+            return response()->json([
+                'message' => 'Terjadi Kesalahan',
+                'data' => null
+            ], 500);
+        }
+    }
+}

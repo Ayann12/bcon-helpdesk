db6fb8f309250c0446a0d125ee1a61df6132b9cc
Ayann12 <tantowi818@gmail.com>
2026-09-11 14:12:20 +0700

    add function ticket show

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -48,6 +48,35 @@ class TicketController extends Controller
         }
     }
 
+    public function show($code)
+    {
+        try {
+            $ticket = Ticket::where('code', $code)->first();
+
+            if (!$ticket) {
+                return response()->json([
+                    'message' => 'Tiket tidak ditemukan'
+                ], 404);
+            }
+
+            if (auth()->user()->role == 'user' && $ticket->user_id != auth()->user()->id) {
+                return response()->json([
+                    'message' => 'Anda tidak diperbolehkan mengakses tiket ini'
+                ], 403);
+            }
+
+            return response()->json([
+                'message' => 'Tiket berhasil ditampilkan',
+                'data' => new TicketResource($ticket)
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Terjadi Kesalahan',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
     public function store(TicketStoreRequest $request)
     {
         $data = $request->validated();
